c7ebf3203465d8ce3ef4dc8c217b21de2b1a1361
Johan Axfors <johan@axfors.se>
2017-02-01 23:07:38 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -20,15 +20,13 @@ if [ ! -f /app/yandex.key ]; then
     echo "4trnsl.X.X.20170201T090054Z.XXXXXXXXXXXXX.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" > /app/yandex.key
 fi
 
-if [ ! -f /app/src/subTranslater.py.org ]; then
-   cp /app/src/subTranslater.py /app/src/subTranslater.py.org
-fi
+#if [ ! -f /app/src/subTranslater.py.org ]; then
+#   cp /app/src/subTranslater.py /app/src/subTranslater.py.org
+#fi
 
 #sed -e "s/YANDEX_API_KEY = ""/YANDEX_API_KEY = "$(< /app/yandex.key)"/" /app/src/subTranslater.py > /app/src/subTranslater.py.tmp
 #mv /app/src/subTranslater.py.tmp /app/src/subTranslater.py
 
-sed -i '10s/.*/ YANDEX_API_KEY = "$(< /app/yandex.key)" /' app/src/subTranslater.py
+#sed -i '10s/.*/ YANDEX_API_KEY = "$(< /app/yandex.key)" /' app/src/subTranslater.py
 
-cat /app/src/subTranslater.py
-
-python /app/run.py "$(< /app/st.cfg)"
+python /app/run.py /srt/The.Hateful.Eight.2015.BDRip.x264-SPARKS/ "$(< /app/st.cfg)"
