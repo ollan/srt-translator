17dd212546e22483a711d186a4998e833eaebf9e
Johan Axfors <johan@axfors.se>
2017-02-01 23:12:07 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -29,4 +29,5 @@ fi
 
 #sed -i '10s/.*/ YANDEX_API_KEY = "$(< /app/yandex.key)" /' app/src/subTranslater.py
 
-python /app/run.py /srt/The.Hateful.Eight.2015.BDRip.x264-SPARKS/ $(< /app/st.cfg)
+#python /app/run.py /srt/The.Hateful.Eight.2015.BDRip.x264-SPARKS/ $(< /app/st.cfg)
+python /app/run.py /srt/The.Hateful.Eight.2015.BDRip.x264-SPARKS/ 40 yandex en sv
