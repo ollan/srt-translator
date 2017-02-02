45cff74a0a6d1cfaa1ea900190fe24848f373735
Johan Axfors <johan@axfors.se>
2017-02-02 09:43:15 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -18,6 +18,8 @@ if [ ! -f /app/src/subTranslater.py.org ]; then
    cp /app/src/subTranslater.py /app/src/subTranslater.py.org
 fi
 
+cp /app/src/subTranslater.py.org /app/src/subTranslater.py
+
 YAK=$(cat "/app/yandex.key")
 sed "s/#YAK/YANDEX_API_KEY = $YAK/g" /app/src/subTranslater.py > /app/src/subTranslater.py.new
 mv /app/src/subTranslater.py.new /app/src/subTranslater.py
