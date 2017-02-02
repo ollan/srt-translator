b7010aae675306f2da0fdd1061ff16636e12e9a1
Johan Axfors <johan@axfors.se>
2017-02-02 15:13:27 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -31,7 +31,10 @@ while inotifywait -e modify -e moved_to -e create /srt; do
          echo "Already translated dir $dirpath"
       else
          echo "Translating subtitle file $filepath"
-         python /app/src/run.py "$dirpath" $(< /app/config/st.cfg) > /dev/null
+         encoding=`file -i "$filepath" | cut -f 2 -d";" | cut -f 2 -d=`
+         echo "Encoding: $encoding"
+         iconv -f "$encoding" -t utf-8 $filepath > $filepath.utf8
+         #python /app/src/run.py "$dirpath" $(< /app/config/st.cfg) > /dev/null
          chmod -R 0777 "$dirpath"
       fi
    done
