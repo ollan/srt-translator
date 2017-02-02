1bf8b473fc15932162eca9769e5726f77851f46c
Johan Axfors <johan@axfors.se>
2017-02-02 11:02:42 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,25 +8,25 @@ git pull /app
 
 if [ ! -f /app/st.cfg ]; then
     echo "40 yandex en sv" > /app/st.cfg
-    echo "Enter your SubTranslater arguments in /app/st.cfg\n"
+    echo "Enter your SubTranslater arguments in /app/st.cfg"
     exit
 fi
 
 if [ ! -f /app/yandex.key ]; then
     echo "4trnsl.X.X.20170201T090054Z.XXXXXXXXXXXXX.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" > /app/yandex.key
-    echo "Enter your Yandex key in /app/yandex.key\n"
+    echo "Enter your Yandex key in /app/yandex.key"
     exit
 fi
 
 YAK=$(cat "/app/yandex.key")
-echo "Yandex key: $YAK\n"
+echo "Yandex key: $YAK"
 sed -i "13s/.*/YANDEX_API_KEY = \"$YAK\"/" /app/src/subTranslater.py
 
 find /srt -name '*.srt' | while read filepath; do
     if [ ! -f "$(dirname "$filepath")/*_to_*.srt" ]; then
-      echo "Processing directory $(dirname "$filepath")\n"
+      echo "Processing directory $(dirname "$filepath")"
       python /app/run.py "$(dirname "$filepath")" $(< /app/st.cfg)
     else
-       echo "Already processed directory $(dirname "$filepath")\n"
+       echo "Already processed directory $(dirname "$filepath")"
     fi
 done
