2dbf327d60c2fb8c485f7161f2c5007c854c0f75
Johan Axfors <johan@axfors.se>
2017-02-02 09:52:39 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,21 +8,26 @@ git pull /app
 
 if [ ! -f /app/st.cfg ]; then
     echo "40 yandex en sv" > /app/st.cfg
+    echo "Enter your SubTranslater arguments in /app/st.cfg"
+    #cp /app/src/subTranslater.py /app/src/subTranslater.py.org
 fi
 
 if [ ! -f /app/yandex.key ]; then
     echo "4trnsl.X.X.20170201T090054Z.XXXXXXXXXXXXX.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" > /app/yandex.key
+    echo "Enter your Yandex key in /app/yandex.key"
+    #cp /app/src/subTranslater.py /app/src/subTranslater.py.org
 fi
 
-if [ ! -f /app/src/subTranslater.py.org ]; then
-   cp /app/src/subTranslater.py /app/src/subTranslater.py.org
-fi
+#if [ ! -f /app/src/subTranslater.py.org ]; then
+   #cp /app/src/subTranslater.py /app/src/subTranslater.py.org
+#fi
 
-cp /app/src/subTranslater.py.org /app/src/subTranslater.py
+#cp /app/src/subTranslater.py.org /app/src/subTranslater.py
 
 YAK=$(cat "/app/yandex.key")
-sed "s/#YAK/YANDEX_API_KEY = $YAK/g" /app/src/subTranslater.py > /app/src/subTranslater.py.new
-mv /app/src/subTranslater.py.new /app/src/subTranslater.py
+#sed "s/#YAK/YANDEX_API_KEY = $YAK/g" /app/src/subTranslater.py > /app/src/subTranslater.py.new
+#mv /app/src/subTranslater.py.new /app/src/subTranslater.py
+sed -i "11s/.*/YANDEX_API_KEY = $YAK/" file_name
 
 python /app/run.py "/srt/TEST" $(< /app/st.cfg)
 
