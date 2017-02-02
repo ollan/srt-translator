b858447464a8bfc6ed93cb6961052ed8ffc3ee2f
Johan Axfors <johan@axfors.se>
2017-02-02 09:41:06 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,12 +6,6 @@
 cd /app
 git pull /app
 
-#pip install goslate 
-#pip install time 
-#pip install requests 
-#pip install codecs
-#pip install sys
-
 if [ ! -f /app/st.cfg ]; then
     echo "40 yandex en sv" > /app/st.cfg
 fi
@@ -20,17 +14,13 @@ if [ ! -f /app/yandex.key ]; then
     echo "4trnsl.X.X.20170201T090054Z.XXXXXXXXXXXXX.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" > /app/yandex.key
 fi
 
-#if [ ! -f /app/src/subTranslater.py.org ]; then
-#   cp /app/src/subTranslater.py /app/src/subTranslater.py.org
-#fi
+if [ ! -f /app/src/subTranslater.py.org ]; then
+   cp /app/src/subTranslater.py /app/src/subTranslater.py.org
+fi
 
-#sed -e "s/YANDEX_API_KEY = ""/YANDEX_API_KEY = "$(< /app/yandex.key)"/" /app/src/subTranslater.py > /app/src/subTranslater.py.tmp
-#mv /app/src/subTranslater.py.tmp /app/src/subTranslater.py
-
-#sed -i '10s/.*/ #YAK "$(< /app/yandex.key)" /' app/src/subTranslater.py
 YAK=$(cat "/app/yandex.key")
-echo $YAK
 sed "s/#YAK/YANDEX_API_KEY = $YAK/g" /app/src/subTranslater.py > /app/src/subTranslater.py.new
+mv /app/src/subTranslater.py.new /app/src/subTranslater.py
 
 python /app/run.py "/srt/TEST" $(< /app/st.cfg)
 
