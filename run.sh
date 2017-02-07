d23deda1831d7b5ddbb3b791bf5a17cda72e0e36
Johan Axfors <johan@axfors.se>
2017-02-07 20:31:09 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,8 +11,20 @@ git pull /app/src
 
 if [ ! -f /app/config/st.cfg ]; then
     echo "40 yandex en sv" > /app/config/st.cfg
-    echo "Enter your SubTranslater arguments in /app/config/st.cfg"
+    echo "Enter SubTranslater arguments in /app/config/st.cfg"
 fi
+
+args=$(cat "/app/config/st.cfg")
+echo "Arguments: $args"
+
+if [ ! -f /app/config/fsp.cfg ]; then
+    echo "*.en.srt" > /app/config/fsp.cfg
+    echo "Enter SubTranslater file search pattern in /app/config/fsp.cfg"
+fi
+
+fsp=$(cat "/app/config/fsp.cfg")
+echo "File search pattern: $fsp"
+
 
 if [ ! -f /app/config/yandex.key ]; then
     echo "4trnsl.X.X.20170201T090054Z.XXXXXXXXXXXXX.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" > /app/config/yandex.key
@@ -24,17 +36,21 @@ echo "Yandex key: $YAK"
 sed -i "13s/.*/YANDEX_API_KEY = \"$YAK\"/" /app/src/src/subTranslater.py
 
 while inotifywait -r -e modify -e moved_to -e create /srt; do
-   find /srt -name '*.srt' | while read filepath; do
-      dirpath=$(dirname "$filepath")
-
+   find /srt -name "$fsp" | while read filepath; do
+      
+      dirpath="$(dirname "$filepath")/translated"
+      filename=$(filename "$filepath")
+      echo "Directory: $dirpath"
+      echo "File name: $filename"
+      
       if [ 0 -lt $(ls "$dirpath"/*_*_to_*.srt 2>/dev/null | wc -w) ]; then
          echo "Already translated dir $dirpath"
       else
          echo "Translating subtitle file $filepath"
          encoding=`file -i "$filepath" | cut -f 2 -d";" | cut -f 2 -d=`
-         mv "$filepath" "$filepath.old"
-         iconv -f "$encoding" -t utf-8 "$filepath.old" > $filepath
-         python /app/src/run.py "$dirpath" $(< /app/config/st.cfg) > /dev/null
+         iconv -f "$encoding" -t utf-8 "$filepath" > "$dirpath/$filename"
+         python /app/src/run.py "$dirpath" "$args" > /dev/null
+         rm "$dirpath/$filename"
          chmod -R 0777 "$dirpath"
       fi
    done
