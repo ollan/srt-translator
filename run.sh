751af2c70386ef284179331d62c7cfa5ddbc5fed
Johan Axfors <johan@axfors.se>
2017-02-13 21:01:41 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -42,13 +42,11 @@ while inotifywait -r -e modify -e moved_to -e create -e delete /srt; do
    find /srt -name "$fsp" | while read filepath; do
       
       dirpath="$(dirname "$filepath")"
-      srtfile="$(basename $dirpath).sv.srt"
-      dirpath="$dirpath/translated"
-      #filename=$(filename "$filepath")
       echo "Directory: $dirpath"
-      echo "SRT file: $srtfile"
-      #echo "File name: $filename"
+      srtfile="$(basename $dirpath).sv.srt"
+      dirpath="$dirpath/srttranslator"
       mkdir -p "$dirpath"
+      echo "Output SRT file: $srtfile"
       
       if [ 0 -lt $(ls "$dirpath"/*_*_to_*.srt 2>/dev/null | wc -w) ]; then
          echo "Already translated directory: $dirpath"
@@ -56,11 +54,11 @@ while inotifywait -r -e modify -e moved_to -e create -e delete /srt; do
          echo "Translating subtitle file: $filepath"
          encoding=`file -i "$filepath" | cut -f 2 -d";" | cut -f 2 -d=`
          echo "Encoding: $encoding"
-         iconv -f "$encoding" -t utf-8 "$filepath" > "$dirpath/utf8.srt"
+         iconv -f "$encoding" -t utf-8 "$filepath" > "$dirpath/srttranslator.srt"
          python /app/src/run.py "$dirpath" $args > /dev/null
-         rm "$dirpath/utf8.srt"
-         
+         rm "$dirpath/srttranslator.srt"
          chmod -R 0777 "$dirpath"
+         cp "$dirpath/*.srt" "../$srtfile"
       fi
    done
    sleep 60
