a8c5df580caaba7ce71b321e046b044b1a9173b1
Johan Axfors <johan@axfors.se>
2017-08-14 08:35:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -11,7 +11,6 @@ git pull /app/src
 
 sed -i "8s/.*/"reload\(sys\)"/" /app/src/src/subTranslater.py
 sed -i "9s/.*/"sys.setdefaultencoding\(\'utf8\'\)"/" /app/src/src/subTranslater.py
-sed -i "104s/json/plain/" /app/src/src/subTranslater.py
 
 if [ ! -f /app/config/st.cfg ]; then
     echo "40 yandex en sv" > /app/config/st.cfg
